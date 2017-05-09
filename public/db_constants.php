1f97112d409fbaa70fcaf961da5572b294ec2dd3
sureshverity <vsuresh.pbp@gmail.com>
2017-05-09 20:14:22 +0530

    Added oublic Folder

---
+++ public/db_constants.php
@@ -0,0 +1,14 @@
+<?php
+	           defined('SENTRIFUGO_HOST') || define('SENTRIFUGO_HOST','localhost');
+	           defined('SENTRIFUGO_USERNAME') || define('SENTRIFUGO_USERNAME','root');
+	           defined('SENTRIFUGO_PASSWORD') || define('SENTRIFUGO_PASSWORD','');
+	          // defined('SENTRIFUGO_DBNAME') || define('SENTRIFUGO_DBNAME','vhris_20-3-2017');
+	           defined('SENTRIFUGO_DBNAME') || define('SENTRIFUGO_DBNAME','vhris');
+
+			   defined('ATTENDANCE_HOST') || define('ATTENDANCE_HOST','192.168.5.12\HRVIEWSERVER');
+	           defined('ATTENDANCE_USERNAME') || define('ATTENDANCE_USERNAME','sa');
+	           defined('ATTENDANCE_PASSWORD') || define('ATTENDANCE_PASSWORD','idcube_hrviewserver2005');
+	           defined('ATTENDANCE_DBNAME') || define('ATTENDANCE_DBNAME','HRVIEW_ACTA');
+                   defined('ATTENDANCE_DATEFORMATE') || define('ATTENDANCE_DATEFORMATE','Y-m-d H:i:s');
+	           
+	         ?>
---
+++ public/mail_settings_constants.php
@@ -0,0 +1,8 @@
+<?php
+		           defined('MAIL_SMTP') || define('MAIL_SMTP','mail.sslverity.com');
+		           defined('MAIL_USERNAME') || define('MAIL_USERNAME','support.vhris@verityinfosol.com');
+		           defined('MAIL_PASSWORD') || define('MAIL_PASSWORD','Verity1@89');
+		           defined('MAIL_PORT') || define('MAIL_PORT','587');
+		           defined('MAIL_AUTH') || define('MAIL_AUTH','true');
+		           defined('MAIL_TLS') || define('MAIL_TLS','tls');
+		         ?>
