ac343435b26f9d82f847f73f020fd3b216ff26fc
sureshverity <vsuresh.pbp@gmail.com>
2017-05-11 19:39:03 +0530

    Added Style to Forms

--- public/db_constants.php
+++ public/db_constants.php
@@ -5,7 +5,7 @@
 	          // defined('SENTRIFUGO_DBNAME') || define('SENTRIFUGO_DBNAME','vhris_20-3-2017');
 	           defined('SENTRIFUGO_DBNAME') || define('SENTRIFUGO_DBNAME','vhris');
 
-			   defined('ATTENDANCE_HOST') || define('ATTENDANCE_HOST','192.168.5.12\HRVIEWSERVER');
+			   defined('ATTENDANCE_HOST') || define('ATTENDANCE_HOST','192.168.5.14\HRVIEWSERVER');
 	           defined('ATTENDANCE_USERNAME') || define('ATTENDANCE_USERNAME','sa');
 	           defined('ATTENDANCE_PASSWORD') || define('ATTENDANCE_PASSWORD','idcube_hrviewserver2005');
 	           defined('ATTENDANCE_DBNAME') || define('ATTENDANCE_DBNAME','HRVIEW_ACTA');
